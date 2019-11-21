8d829b544dae6ef230d5d75b53ae100d2b533e18
Bart Grispen <bartstarss@gmail.com>
2019-11-21 16:24:53 +0100

    Added projects folder

---
+++ PSoC/Projects/main_BLE.c
@@ -0,0 +1,684 @@
+#include "project.h"
+#include <mpu6050.h>
+#include <stdio.h>
+#include <math.h>
+
+#define LED_LENGTH 104
+
+int mode;
+int16_t ax, ay, az, i;
+	int16_t gx, gy, gz;
+
+typedef struct _RGB_tag{
+	unsigned char g;
+	unsigned char r;
+	unsigned char b;
+} rgb_tag;
+
+rgb_tag rgb[LED_LENGTH];
+unsigned int tick, current_led;
+
+void Stack_Handler();
+void Ias_Handler();
+void BLE_Update();
+void isr_systick();
+void isr_reset();
+void isr_fifo_empty();
+void transfer_LEDdriver();
+void set_next_color();
+
+
+int main(void)
+{
+     for(;;)
+    {
+    CyGlobalIntEnable;
+    CyBle_Start( Stack_Handler );
+    CyBle_IasRegisterAttrCallback( Ias_Handler );
+    CyBle_ProcessEvents();
+    char buf[50]; //just to hold text values in for writing to UART
+
+    
+	I2C_MPU6050_Start();
+	SERIAL_Start();
+	
+    //CyGlobalIntEnable;
+    //CyBle_Start( Stack_Handler );
+    //:CyBle_IasRegisterAttrCallback( Ias_Handler );
+    //BLE_Update(); //Moet gebruikt worden, maar is een for(;;) loop
+    
+	MPU6050_init();
+	MPU6050_initialize();
+	SERIAL_UartPutString(MPU6050_testConnection() ? "MPU6050 connection successful\n\r" : "MPU6050 connection failed\n\n\r");
+    
+    SERIAL_UartPutString("Raw values from gyroscope..\n\r");
+    isr_fifo_empty_StartEx(isr_fifo_empty);
+    isr_reset_done_StartEx(isr_reset);
+    
+    // configure systick for interrupt each 1ms
+    CyIntSetSysVector(15,isr_systick);
+    if (SysTick_Config( (40000000) / 1000)) { 
+            while (1);  //Capture error
+    }
+    
+    // start component
+    Counter_reset_gen_Start() ;
+        
+    CyGlobalIntEnable;
+    
+    // wait for dummy interrupt is triggered.
+  
+    while(1)
+    {
+      MPU6050_getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      
+      sprintf(buf, "AX:%d, AY:%d, AZ:%d || GX:%d, GY:%d, GZ:%d,\t", ax,ay,az,gx,gy,gz);
+      SERIAL_UartPutString(buf);
+      SERIAL_UartPutString("\n\r");
+      CyDelay(300);
+    }
+    }
+    
+}
+void BLE_Update()
+{
+    for(;;)
+    {
+     CyBle_ProcessEvents();
+    }
+}
+
+void isr_systick(){
+    tick++;
+    if( (tick&0xf) == 0 ){
+        set_next_color();   // exec each 16ms
+    }
+}
+
+// transfer & reset done
+void isr_reset(){
+    //transfer_LEDdriver();
+}
+
+// fifo empty. fill fifo.
+void isr_fifo_empty(){
+    if(current_led){
+        transfer_LEDdriver();
+    }
+}
+
+// start transfer
+void transfer_LEDdriver(){
+    unsigned char len;
+    if(current_led+3<LED_LENGTH){
+        WS2812driver_1_write2fifo((unsigned char*)&rgb[current_led], 9);
+        current_led += 3;
+    }else{
+        len = LED_LENGTH - current_led;
+        WS2812driver_1_write2fifo((unsigned char*)&rgb[current_led], len * 3);
+        current_led = 0;
+    }
+}
+
+void Stack_Handler( uint32 eventCode, void *eventParam)
+{
+      switch (eventCode)
+      {
+        case CYBLE_EVT_STACK_ON:
+        case CYBLE_EVT_GAP_DEVICE_DISCONNECTED:
+               CyBle_GappStartAdvertisement( CYBLE_ADVERTISING_FAST);
+            break;
+       }
+}
+
+void Ias_Handler ( uint32 eventCode, void *eventParam, int mode )
+{
+    CYBLE_IAS_CHAR_VALUE_T *param = (CYBLE_IAS_CHAR_VALUE_T *) eventParam;
+    if( *param->value->val == 0)
+    {
+        int mode = 0;
+    }
+    else if( *param->value->val == 1)
+    {
+        int mode = 1;
+    }
+    else
+    {
+        int mode = 2;
+    }
+}
+
+
+// exec each 16ms
+void set_next_color(){
+    switch(mode){
+        case 0: //RGB Mode
+        {
+            static unsigned char val;
+            int i;
+          
+            // shift
+            for(i=LED_LENGTH-1;i>0;i--){
+                rgb[i].r = rgb[i-1].r;
+                rgb[i].g = rgb[i-1].g;
+                rgb[i].b = rgb[i-1].b;
+            }
+    
+            if(val<32){
+                rgb[0].r = val;
+                rgb[0].g = 0;
+                rgb[0].b = 32 - val;
+            }else if(val<64){
+                rgb[0].r = 64 - val;
+                rgb[0].g = val - 32;
+                rgb[0].b = 0;
+            }else{
+                rgb[0].r = 0;
+                rgb[0].g = 96 - val;
+                rgb[0].b = val - 64;
+            }
+    
+            transfer_LEDdriver();
+        
+            if(val<96){
+                val++;
+            }else{
+                val = 0;
+            }
+            break;
+        }
+        case 1:
+        {
+            rgb[0].r = 255;
+            rgb[1].r = 255;
+            rgb[2].r = 255;
+            rgb[3].r = 255;
+            rgb[4].r = 255;
+            rgb[5].r = 255;
+            rgb[6].r = 255;
+            rgb[7].r = 255;
+            rgb[8].r = 255;
+            rgb[9].r = 255;
+            rgb[10].r = 255;
+            rgb[11].r = 255;
+            rgb[12].r = 255;
+            rgb[13].b = 255;
+            rgb[14].b = 255;
+            rgb[15].b = 255;
+            rgb[16].b = 255;
+            rgb[17].b = 255;
+            rgb[18].b = 255;
+            rgb[19].b = 255;
+            rgb[20].b = 255;
+            rgb[21].b = 255;
+            rgb[22].b = 255;
+            rgb[23].b = 255;
+            rgb[24].b = 255;
+            rgb[25].b = 255;
+            rgb[26].g = 255;
+            rgb[27].g = 255;
+            rgb[28].g = 255;
+            rgb[29].g = 255;
+            rgb[30].g = 255;
+            rgb[31].g = 255;
+            rgb[32].g = 255;
+            rgb[33].g = 255;
+            rgb[34].g = 255;
+            rgb[35].g = 255;
+            rgb[36].g = 255;
+            rgb[37].g = 255;
+            rgb[38].g = 255;
+            rgb[39].r = 255;
+            rgb[40].r = 255;
+            rgb[41].r = 255;
+            rgb[42].r = 255;
+            rgb[43].r = 255;
+            rgb[44].r = 255;
+            rgb[45].r = 255;
+            rgb[46].r = 255;
+            rgb[47].r = 255;
+            rgb[48].r = 255;
+            rgb[49].r = 255;
+            rgb[50].r = 255;
+            rgb[51].r = 255;
+            rgb[53].b = 255;
+            rgb[54].b = 255;
+            rgb[55].b = 255;
+            rgb[56].b = 255;
+            rgb[57].b = 255;
+            rgb[58].b = 255;
+            rgb[59].b = 255;
+            rgb[60].b = 255;
+            rgb[61].b = 255;
+            rgb[62].b = 255;
+            rgb[63].b = 255;
+            rgb[64].b = 255;
+            rgb[65].g = 255;
+            rgb[66].g = 255;
+            rgb[67].g = 255;
+            rgb[68].g = 255;
+            rgb[69].g = 255;
+            rgb[70].g = 255;
+            rgb[71].g = 255;
+            rgb[72].g = 255;
+            rgb[73].g = 255;
+            rgb[74].g = 255;
+            rgb[75].g = 255;
+            rgb[76].g = 255;
+            rgb[77].g = 255;
+            rgb[70].g = 255;
+            transfer_LEDdriver();
+            
+            
+            break;
+        }
+        case 2:
+        if (ay < 1500)
+        {
+            rgb[0].r = 100;
+            rgb[1].r = 100;
+            rgb[2].r = 100;
+            rgb[3].r = 100;
+            rgb[4].r = 100;
+            rgb[5].r = 100;
+            rgb[6].r = 100;
+            rgb[7].r = 100;
+            rgb[8].r = 100;
+            rgb[9].r = 100;
+            rgb[10].r = 100;
+            rgb[11].r = 100;
+            rgb[12].r = 100;
+            rgb[13].b = 0;
+            rgb[14].b = 0;
+            rgb[15].b = 0;
+            rgb[16].b = 0;
+            rgb[17].b = 0;
+            rgb[18].b = 0;
+            rgb[19].b = 0;
+            rgb[20].b = 0;
+            rgb[21].b = 0;
+            rgb[22].b = 0;
+            rgb[23].b = 0;
+            rgb[24].b = 0;
+            rgb[25].b = 0;
+            rgb[26].g = 0;
+            rgb[27].g = 0;
+            rgb[28].g = 0;
+            rgb[29].g = 0;
+            rgb[30].g = 0;
+            rgb[31].g = 0;
+            rgb[32].g = 0;
+            rgb[33].g = 0;
+            rgb[34].g = 0;
+            rgb[35].g = 0;
+            rgb[36].g = 0;
+            rgb[37].g = 0;
+            rgb[38].g = 0;
+            rgb[39].r = 0;
+            rgb[40].r = 0;
+            rgb[41].r = 0;
+            rgb[42].r = 0;
+            rgb[43].r = 0;
+            rgb[44].r = 0;
+            rgb[45].r = 0;
+            rgb[46].r = 0;
+            rgb[47].r = 0;
+            rgb[48].r = 0;
+            rgb[49].r = 0;
+            rgb[50].r = 0;
+            rgb[51].r = 0;
+            rgb[53].b = 0;
+            rgb[54].b = 0;
+            rgb[55].b = 0;
+            rgb[56].b = 0;
+            rgb[57].b = 0;
+            rgb[58].b = 0;
+            rgb[59].b = 0;
+            rgb[60].b = 0;
+            rgb[61].b = 0;
+            rgb[62].b = 0;
+            rgb[63].b = 0;
+            rgb[64].b = 0;
+            rgb[65].g = 0;
+            rgb[66].g = 0;
+            rgb[67].g = 0;
+            rgb[68].g = 0;
+            rgb[69].g = 0;
+            rgb[70].g = 0;
+            rgb[71].g = 0;
+            rgb[72].g = 0;
+            rgb[73].g = 0;
+            rgb[74].g = 0;
+            rgb[75].g = 0;
+            rgb[76].g = 0;
+            rgb[77].g = 0;
+            rgb[70].g = 0;
+            transfer_LEDdriver();
+        }
+        else if(ay < 3000 && ay < 4500){
+            rgb[0].r = 255;
+            rgb[1].r = 255;
+            rgb[2].r = 255;
+            rgb[3].r = 255;
+            rgb[4].r = 255;
+            rgb[5].r = 255;
+            rgb[6].r = 255;
+            rgb[7].r = 255;
+            rgb[8].r = 255;
+            rgb[9].r = 255;
+            rgb[10].r = 255;
+            rgb[11].r = 255;
+            rgb[12].r = 255;
+            rgb[13].b = 255;
+            rgb[14].b = 255;
+            rgb[15].b = 255;
+            rgb[16].b = 255;
+            rgb[17].b = 255;
+            rgb[18].b = 255;
+            rgb[19].b = 255;
+            rgb[20].b = 255;
+            rgb[21].b = 255;
+            rgb[22].b = 255;
+            rgb[23].b = 255;
+            rgb[24].b = 255;
+            rgb[25].b = 255;
+            rgb[26].g = 0;
+            rgb[27].g = 0;
+            rgb[28].g = 0;
+            rgb[29].g = 0;
+            rgb[30].g = 0;
+            rgb[31].g = 0;
+            rgb[32].g = 0;
+            rgb[33].g = 0;
+            rgb[34].g = 0;
+            rgb[35].g = 0;
+            rgb[36].g = 0;
+            rgb[37].g = 0;
+            rgb[38].g = 0;
+            rgb[39].r = 0;
+            rgb[40].r = 0;
+            rgb[41].r = 0;
+            rgb[42].r = 0;
+            rgb[43].r = 0;
+            rgb[44].r = 0;
+            rgb[45].r = 0;
+            rgb[46].r = 0;
+            rgb[47].r = 0;
+            rgb[48].r = 0;
+            rgb[49].r = 0;
+            rgb[50].r = 0;
+            rgb[51].r = 0;
+            rgb[53].b = 0;
+            rgb[54].b = 0;
+            rgb[55].b = 0;
+            rgb[56].b = 0;
+            rgb[57].b = 0;
+            rgb[58].b = 0;
+            rgb[59].b = 0;
+            rgb[60].b = 0;
+            rgb[61].b = 0;
+            rgb[62].b = 0;
+            rgb[63].b = 0;
+            rgb[64].b = 0;
+            rgb[65].g = 0;
+            rgb[66].g = 0;
+            rgb[67].g = 0;
+            rgb[68].g = 0;
+            rgb[69].g = 0;
+            rgb[70].g = 0;
+            rgb[71].g = 0;
+            rgb[72].g = 0;
+            rgb[73].g = 0;
+            rgb[74].g = 0;
+            rgb[75].g = 0;
+            rgb[76].g = 0;
+            rgb[77].g = 0;
+            rgb[70].g = 0;
+        transfer_LEDdriver();   
+        }
+        else if(ay < 6000 && ay < 7500){
+            rgb[0].r = 255;
+            rgb[1].r = 255;
+            rgb[2].r = 255;
+            rgb[3].r = 255;
+            rgb[4].r = 255;
+            rgb[5].r = 255;
+            rgb[6].r = 255;
+            rgb[7].r = 255;
+            rgb[8].r = 255;
+            rgb[9].r = 255;
+            rgb[10].r = 255;
+            rgb[11].r = 255;
+            rgb[12].r = 255;
+            rgb[13].b = 255;
+            rgb[14].b = 255;
+            rgb[15].b = 255;
+            rgb[16].b = 255;
+            rgb[17].b = 255;
+            rgb[18].b = 255;
+            rgb[19].b = 255;
+            rgb[20].b = 255;
+            rgb[21].b = 255;
+            rgb[22].b = 255;
+            rgb[23].b = 255;
+            rgb[24].b = 255;
+            rgb[25].b = 255;
+            rgb[26].g = 255;
+            rgb[27].g = 255;
+            rgb[28].g = 255;
+            rgb[29].g = 255;
+            rgb[30].g = 255;
+            rgb[31].g = 255;
+            rgb[32].g = 255;
+            rgb[33].g = 255;
+            rgb[34].g = 255;
+            rgb[35].g = 255;
+            rgb[36].g = 255;
+            rgb[37].g = 255;
+            rgb[38].g = 255;
+            rgb[39].r = 0;
+            rgb[40].r = 0;
+            rgb[41].r = 0;
+            rgb[42].r = 0;
+            rgb[43].r = 0;
+            rgb[44].r = 0;
+            rgb[45].r = 0;
+            rgb[46].r = 0;
+            rgb[47].r = 0;
+            rgb[48].r = 0;
+            rgb[49].r = 0;
+            rgb[50].r = 0;
+            rgb[51].r = 0;
+            rgb[53].b = 0;
+            rgb[54].b = 0;
+            rgb[55].b = 0;
+            rgb[56].b = 0;
+            rgb[57].b = 0;
+            rgb[58].b = 0;
+            rgb[59].b = 0;
+            rgb[60].b = 0;
+            rgb[61].b = 0;
+            rgb[62].b = 0;
+            rgb[63].b = 0;
+            rgb[64].b = 0;
+            rgb[65].g = 0;
+            rgb[66].g = 0;
+            rgb[67].g = 0;
+            rgb[68].g = 0;
+            rgb[69].g = 0;
+            rgb[70].g = 0;
+            rgb[71].g = 0;
+            rgb[72].g = 0;
+            rgb[73].g = 0;
+            rgb[74].g = 0;
+            rgb[75].g = 0;
+            rgb[76].g = 0;
+            rgb[77].g = 0;
+            rgb[70].g = 0;
+        transfer_LEDdriver();   
+        }
+        else if(ay < 9000 && ay < 10500){
+            rgb[0].r = 255;
+            rgb[1].r = 255;
+            rgb[2].r = 255;
+            rgb[3].r = 255;
+            rgb[4].r = 255;
+            rgb[5].r = 255;
+            rgb[6].r = 255;
+            rgb[7].r = 255;
+            rgb[8].r = 255;
+            rgb[9].r = 255;
+            rgb[10].r = 255;
+            rgb[11].r = 255;
+            rgb[12].r = 255;
+            rgb[13].b = 255;
+            rgb[14].b = 255;
+            rgb[15].b = 255;
+            rgb[16].b = 255;
+            rgb[17].b = 255;
+            rgb[18].b = 255;
+            rgb[19].b = 255;
+            rgb[20].b = 255;
+            rgb[21].b = 255;
+            rgb[22].b = 255;
+            rgb[23].b = 255;
+            rgb[24].b = 255;
+            rgb[25].b = 255;
+            rgb[26].g = 255;
+            rgb[27].g = 255;
+            rgb[28].g = 255;
+            rgb[29].g = 255;
+            rgb[30].g = 255;
+            rgb[31].g = 255;
+            rgb[32].g = 255;
+            rgb[33].g = 255;
+            rgb[34].g = 255;
+            rgb[35].g = 255;
+            rgb[36].g = 255;
+            rgb[37].g = 255;
+            rgb[38].g = 255;
+            rgb[39].r = 255;
+            rgb[40].r = 255;
+            rgb[41].r = 255;
+            rgb[42].r = 255;
+            rgb[43].r = 255;
+            rgb[44].r = 255;
+            rgb[45].r = 255;
+            rgb[46].r = 255;
+            rgb[47].r = 255;
+            rgb[48].r = 255;
+            rgb[49].r = 255;
+            rgb[50].r = 255;
+            rgb[51].r = 255;
+            rgb[53].b = 0;
+            rgb[54].b = 0;
+            rgb[55].b = 0;
+            rgb[56].b = 0;
+            rgb[57].b = 0;
+            rgb[58].b = 0;
+            rgb[59].b = 0;
+            rgb[60].b = 0;
+            rgb[61].b = 0;
+            rgb[62].b = 0;
+            rgb[63].b = 0;
+            rgb[64].b = 0;
+            rgb[65].g = 0;
+            rgb[66].g = 0;
+            rgb[67].g = 0;
+            rgb[68].g = 0;
+            rgb[69].g = 0;
+            rgb[70].g = 0;
+            rgb[71].g = 0;
+            rgb[72].g = 0;
+            rgb[73].g = 0;
+            rgb[74].g = 0;
+            rgb[75].g = 0;
+            rgb[76].g = 0;
+            rgb[77].g = 0;
+            rgb[70].g = 0;
+        transfer_LEDdriver();   
+        }
+        else{
+            rgb[0].r = 0;
+            rgb[1].r = 0;
+            rgb[2].r = 0;
+            rgb[3].r = 0;
+            rgb[4].r = 0;
+            rgb[5].r = 0;
+            rgb[6].r = 0;
+            rgb[7].r = 0;
+            rgb[8].r = 0;
+            rgb[9].r = 0;
+            rgb[10].r = 0;
+            rgb[11].r = 0;
+            rgb[12].r = 0;
+            rgb[13].b = 0;
+            rgb[14].b = 0;
+            rgb[15].b = 0;
+            rgb[16].b = 0;
+            rgb[17].b = 0;
+            rgb[18].b = 0;
+            rgb[19].b = 0;
+            rgb[20].b = 0;
+            rgb[21].b = 0;
+            rgb[22].b = 0;
+            rgb[23].b = 0;
+            rgb[24].b = 0;
+            rgb[25].b = 0;
+            rgb[26].g = 0;
+            rgb[27].g = 0;
+            rgb[28].g = 0;
+            rgb[29].g = 0;
+            rgb[30].g = 0;
+            rgb[31].g = 0;
+            rgb[32].g = 0;
+            rgb[33].g = 0;
+            rgb[34].g = 0;
+            rgb[35].g = 0;
+            rgb[36].g = 0;
+            rgb[37].g = 0;
+            rgb[38].g = 0;
+            rgb[39].r = 0;
+            rgb[40].r = 0;
+            rgb[41].r = 0;
+            rgb[42].r = 0;
+            rgb[43].r = 0;
+            rgb[44].r = 0;
+            rgb[45].r = 0;
+            rgb[46].r = 0;
+            rgb[47].r = 0;
+            rgb[48].r = 0;
+            rgb[49].r = 0;
+            rgb[50].r = 0;
+            rgb[51].r = 0;
+            rgb[53].b = 0;
+            rgb[54].b = 0;
+            rgb[55].b = 0;
+            rgb[56].b = 0;
+            rgb[57].b = 0;
+            rgb[58].b = 0;
+            rgb[59].b = 0;
+            rgb[60].b = 0;
+            rgb[61].b = 0;
+            rgb[62].b = 0;
+            rgb[63].b = 0;
+            rgb[64].b = 0;
+            rgb[65].g = 0;
+            rgb[66].g = 0;
+            rgb[67].g = 0;
+            rgb[68].g = 0;
+            rgb[69].g = 0;
+            rgb[70].g = 0;
+            rgb[71].g = 0;
+            rgb[72].g = 0;
+            rgb[73].g = 0;
+            rgb[74].g = 0;
+            rgb[75].g = 0;
+            rgb[76].g = 0;
+            rgb[77].g = 0;
+            rgb[70].g = 0;
+            transfer_LEDdriver();
+        }
+    }
+
+}
